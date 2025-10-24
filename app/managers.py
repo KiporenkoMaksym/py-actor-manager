2ebd2d59c4c1e43618ed30b03bc8c35dc71c2300
Maksym Kiporenko <kiporenkomaks8@gmail.com>
2025-10-24 18:52:23 +0300

    complete4

--- app/managers.py
+++ app/managers.py
@@ -4,11 +4,15 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name: str = "actors_db.sqlite", table_name: str = "actors") -> None:
+    def __init__(self,
+                 db_name: str = "actors_db.sqlite",
+                 table_name: str = "actors") -> None:
         self._connection = sqlite3.connect(db_name)
         self.table_name = table_name
 
-    def create(self, first_name: str, last_name: str) -> None:
+    def create(self,
+               first_name: str,
+               last_name: str) -> None:
         self._connection.execute(
             f"INSERT INTO {self.table_name } (first_name, last_name) VALUES (?, ?)",
             (first_name, last_name,)
